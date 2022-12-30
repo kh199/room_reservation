49018fce90e467f37c48cca2de8eab9e6621f70d
Екатерина Каричева <yek.cari4ewa@yandex.ru>
2022-12-30 23:11:39 +0300

    Add database logic

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 
+from app.api.meeting_room import router
+from app.core.config import settings
 from app.core.config import settings
 
 
 app = FastAPI(title=settings.app_title, description=settings.app_description)
+
+app.include_router(router)
