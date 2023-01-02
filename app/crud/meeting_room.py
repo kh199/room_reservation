f7f59aa75c42e60c8fc888e3b8b4cff17627da59
Екатерина Каричева <yek.cari4ewa@yandex.ru>
2023-01-02 23:48:58 +0300

    Add update

--- app/crud/meeting_room.py
+++ app/crud/meeting_room.py
@@ -1,11 +1,12 @@
 from typing import Optional
-
+from fastapi.encoders import jsonable_encoder
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.db import AsyncSessionLocal
 from app.models.meeting_room import MeetingRoom
-from app.schemas.meeting_room import MeetingRoomCreate
+from app.schemas.meeting_room import MeetingRoomCreate, MeetingRoomUpdate
+
 
 
 async def create_meeting_room(
@@ -51,4 +52,21 @@ async def get_meeting_room_by_id(
         )
     )
     db_room = db_room.scalars().first()
+    return db_room
+
+
+async def update_meeting_room(
+        db_room: MeetingRoom,
+        room_in: MeetingRoomUpdate,
+        session: AsyncSession,
+) -> MeetingRoom:
+    obj_data = jsonable_encoder(db_room)
+    update_data = room_in.dict(exclude_unset=True)
+
+    for field in obj_data:
+        if field in update_data:
+            setattr(db_room, field, update_data[field])
+    session.add(db_room)
+    await session.commit()
+    await session.refresh(db_room)
     return db_room
